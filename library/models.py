6e35c5ee3dc90b9c31e2383e7fbc5a00efc29db8
AnastasiiaProskurenko <proskurenkoanastasiya@gmail.com>
2025-11-06 12:33:34 +0100

    create model book

--- library/models.py
+++ library/models.py
@@ -3,6 +3,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
+
 class Author(models.Model):
     first_name = models.CharField(max_length=20, help_text="First name of the author", verbose_name="Имя автора")
     last_name = models.CharField(max_length=50, help_text="Last name of the author", verbose_name="Фамилия автора")
@@ -18,3 +19,14 @@ class Author(models.Model):
 
     def __str__(self):
         return f"{self.last_name[0]}.{self.first_name}"
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publication_date = models.DateField(blank=True, null=True)
+    author = models.ForeignKey(
+        "Author", on_delete=models.SET_NULL, null=True,
+        related_name="books"
+    )
+
+    def __str__(self):
+        return f"{self.title} --{self.author.last_name if self.author egit lse 'NONAME'}"
